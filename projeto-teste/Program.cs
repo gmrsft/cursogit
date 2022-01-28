40a3a45e8752ca6e114a6fe48480f7ef8b3f964b
Guilherme Mathias <admguimat@gmail.com>
2022-01-27 21:12:04 -0300

    Acrescentada a msg git teste

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,7 +7,7 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Consoloe.WriteLine("TesteGuit");
            
         }
     }
